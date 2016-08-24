5e5715a20ec0fcad41003c3d624c116915a5f174
Ruben Maurer <rubenmaurer@live.de>
2016-08-24 20:28:43 +0200

    Change PACE CSS
    Changed the size of the PACE loading bar

--- js/util.js
+++ js/util.js
@@ -1,3 +1,9 @@
 String.prototype.capitalizeFirstLetter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
+
+window.paceOptions = {
+    ajax: {
+        trackMethods: ['GET', 'POST']
+    }
+}
