41f918e0678da3aa0e9bee6b17466b386bf07563
Ruben Maurer <rubenmaurer@live.de>
2016-09-27 17:30:47 +0200

    Changed Gallery & Translation
    - Changed the gallerys design
     - The current selected wallpaper & sound is highlighted
    - Fixed an error in the translation system

--- js/action.js
+++ js/action.js
@@ -35,18 +35,39 @@ $( document ).ready(function() {
         loadLanguage();
     });
 
-    $( '.thumbnail' ).click(function(){
+    $( '.thumbnail-wrapper' ).click(function(){
+        markThumbnail( this );
         var role = $( this ).attr( 'role' );
         var value = $( this ).attr( 'value' );
+        var id = $( this ).attr( 'id' );
 
         if(role == "background"){
-            localStorage.setItem("background", value);
+            store.set( 'background', value );
             setBackground();
             return;
         }
 
         store.set('soundpack', value);
     });
+
+    $('#background-gallery').on('show.bs.modal', function (e) {
+        var id = store.get( 'background' ).split( '.' )[0];
+        var obj = $( '#' + id );
+
+        markThumbnail( obj );
+    })
+
+    $('#sound-gallery').on('show.bs.modal', function (e) {
+        markThumbnail( $( '#' + store.get( 'soundpack' ) + '-sp' ) );
+    })
+
+    function markThumbnail(data){
+        $(".thumbnail-wrapper").each(function() {
+            $( this ).removeClass( 'marked' ); 
+        });
+
+        $( data ).addClass( 'marked' );
+    }
 
     function loadLanguage(){
         if(getParameterByName('lang') != store.get('language')){
