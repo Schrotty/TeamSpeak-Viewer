6124b03fe9116a5bd0ac1adbf767b6e4bd610679
Ruben Maurer <rubenmaurer@live.de>
2016-10-02 02:30:25 +0200

    ReCommit mod system

---
+++ modules/notifications/settings.php
@@ -0,0 +1,6 @@
+<div class="settings-col">
+    <div class="col-md-6"><?php $this->oTranslator->TranslateString("notification-settings") ?></div>
+    <div class="col-md-6">
+        <button type="button" class="btn btn-default" data-toggle="modal" data-target="#sound-gallery"><?php $this->oTranslator->TranslateString("notification-settings-btn") ?></button>
+    </div>
+</div>
